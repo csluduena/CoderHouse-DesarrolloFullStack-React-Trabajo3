156471c8a22eb0533923ffb29ed7d551480cdab1
Carlos Sebastian Ludueña <csluduena@gmail.com>
2024-06-18 06:52:50 -0300

    agregados SEO y Adds

--- src/components/header/NavBar.jsx
+++ src/components/header/NavBar.jsx
@@ -2,10 +2,10 @@ import React, { useEffect, useState } from 'react';
 import { Link, NavLink } from 'react-router-dom';
 import { collection, getDocs } from "firebase/firestore";
 import { db } from "../../firebase/config";
-import { CartWidget } from './CartWidget'
+import { CartWidget } from './CartWidget';
+import '../../css/main.css'; // Asegúrate de importar tu archivo CSS
 
 export const NavBar = () => {
-
     let [categories, setCategories] = useState([]);
 
     useEffect(() => {
@@ -18,16 +18,15 @@ export const NavBar = () => {
             })
     }, [])
 
-
     return (
         <nav style={{ display: 'flex', fontSize: '30px', justifyContent: 'space-between', alignItems: 'center', padding: '10px 20px', backgroundColor: '#333333', color: 'white' }}>
             <div className='mainTitleCFG'>
                 <NavLink to="/" style={{ fontSize: '30px', fontFamily: 'MuseoSans-900' }}>GuitarStore</NavLink>
             </div>
 
-            <div style={{ backgroundColor: '#333333', display: 'flex', alignItems: 'center' }}>
+            <div style={{ display: 'flex', alignItems: 'center' }}>
                 {categories.map((category) => (
-                    <NavLink key={category.id} to={`/category/${category.id}`} className="navRoutes" style={{ fontSize: '30px', margin: '0 10px', color: 'white', textDecoration: 'none' }}>
+                    <NavLink key={category.id} to={`/category/${category.id}`} className="navRoutes" style={{ fontSize: '30px', margin: '0 10px', textDecoration: 'none' }}>
                         {category.nombre}
                     </NavLink >
                 ))}
